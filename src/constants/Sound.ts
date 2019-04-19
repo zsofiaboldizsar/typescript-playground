3f55570bbdbb479773009a917c2c40b2ef0cebb2
zsofia.boldizsar <zsofia.boldizsar@hu.ibm.com>
2019-04-19 10:38:06 +0200

    anmial sounds enum

---
+++ src/constants/Sound.ts
@@ -0,0 +1,8 @@
+export enum Sound {
+    BirdsSound = 'cheep',
+    FishSound = 'click',
+    ReptilesSound = 'hiss',
+    AmphibiansSound = 'croak',
+    MammalsSound = 'moo',
+    InsectsSound = 'buzz'
+}
